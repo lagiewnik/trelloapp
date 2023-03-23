0aa8a62df2c1ec653254992e35194a920ec20e82
lagiewnik <lagiewnik@gmail.com>
2023-03-23 22:28:47 +0100

    Add intercept & stub

--- cypress/e2e/my/boards.cy.ts
+++ cypress/e2e/my/boards.cy.ts
@@ -20,4 +20,42 @@ describe('Boards testing', () => {
             .get('.Star')
             .click();
     });
+
+    it('should create a new board by intercept from fixture', () => {
+        cy.intercept('GET', 'api/boards', { fixture: 'boards' }).as(
+            'getBoards'
+        );
+        cy.visit('/');
+    });
+
+    it('should create a new board by intercept from stub const', () => {
+        const myStub = [
+            {
+              "name": "testowy board z intercepta przez const body",
+              "user": 0,
+              "id": 5928869338,
+              "starred": true,
+              "created": "2023-03-12"
+            },
+            {
+                "name": "Drugi testowy board z intercepta przez const body",
+                "user": 0,
+                "id": 5924469338,
+                "starred": false,
+                "created": "2023-03-12"
+              }
+          ]
+            
+        
+        cy.intercept('GET', 'api/boards', { body: myStub }).as(
+            'getBoards'
+        );
+        cy.visit('/');
+    });
+
+    it('Stub 500 error statusCode', () => {
+        cy.intercept('GET', '/api/boards', {statusCode: 500}).as(
+            'getBoards');
+        cy.visit('/');
+    });
 });
